6b2141764e6073f5d8f213447e4d6a038a1d2e45
Michael McNichol <mcnichol.m@gmail.com>
2016-09-08 07:31:01 -0500

    Test 2: Passed Integer 2, returns String 2 - Failing

--- src/test/java/com/mcnichol/katas/fizzbuzz/FizzBuzzAppTest.java
+++ src/test/java/com/mcnichol/katas/fizzbuzz/FizzBuzzAppTest.java
@@ -9,7 +9,18 @@ public class FizzBuzzAppTest {
     @Test
     public void execute_passedIntegerOne_returnsStringOne() throws Exception {
         FizzBuzzApp subject = new FizzBuzzApp();
+
         String actual = subject.execute(1);
+
         assertThat(actual, equalTo("1"));
+    }
+
+    @Test
+    public void execute_passedIntegerTwo_returnsStringTwo() throws Exception {
+        FizzBuzzApp subject = new FizzBuzzApp();
+
+        String actual = subject.execute(2);
+
+        assertThat(actual, equalTo("2"));
     }
 }
