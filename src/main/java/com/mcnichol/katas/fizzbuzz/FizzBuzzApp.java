58c316ebed730e2beb8838b3c52b577417e32e07
Michael McNichol <mcnichol.m@gmail.com>
2016-09-08 07:28:18 -0500

    Test 1: Passed Integer 1, returns String 1 - Failing

--- src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzApp.java
+++ src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzApp.java
@@ -2,4 +2,7 @@ package com.mcnichol.katas.fizzbuzz;
 
 public class FizzBuzzApp {
 
+    public String execute(Integer userInput) {
+        return null;
+    }
 }
--- src/test/java/com/mcnichol/katas/fizzbuzz/FizzBuzzAppTest.java
+++ src/test/java/com/mcnichol/katas/fizzbuzz/FizzBuzzAppTest.java
@@ -1,7 +1,15 @@
 package com.mcnichol.katas.fizzbuzz;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class FizzBuzzAppTest {
-
+    @Test
+    public void execute_passedIntegerOne_returnsStringOne() throws Exception {
+        FizzBuzzApp subject = new FizzBuzzApp();
+        String actual = subject.execute(1);
+        assertThat(actual, equalTo("1"));
+    }
 }
