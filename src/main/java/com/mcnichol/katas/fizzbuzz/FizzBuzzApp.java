98c40b6dbbd521a510a8fd2fb955823eb0d8a782
Michael McNichol <mcnichol.m@gmail.com>
2016-09-14 07:35:02 -0500

    Test 9: Passed Integer 30, returns FizzBuzz - Passing

--- src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzApp.java
+++ src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzApp.java
@@ -5,7 +5,7 @@ public class FizzBuzzApp {
     public String execute(Integer userInput) {
         String result;
 
-        if (userInput == 15) {
+        if (isInputDivisibleBy(15, userInput)) {
             result = "FizzBuzz";
         } else if (isInputDivisibleBy(3, userInput)) {
             result = "Fizz";
