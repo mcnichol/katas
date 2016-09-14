4a724f80c8cb6762490098c3390b7084104b3e7a
Michael McNichol <mcnichol.m@gmail.com>
2016-09-14 07:23:53 -0500

    Test 6: Passed Integer 6, returns Fizz - Passing

--- src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzApp.java
+++ src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzApp.java
@@ -4,9 +4,10 @@ public class FizzBuzzApp {
 
     public String execute(Integer userInput) {
         String result;
-        if (userInput == 3) {
+
+        if (userInput % 3 == 0) {
             result = "Fizz";
-        } else if(userInput == 5){
+        } else if (userInput == 5) {
             result = "Buzz";
         } else {
             result = String.valueOf(userInput);
