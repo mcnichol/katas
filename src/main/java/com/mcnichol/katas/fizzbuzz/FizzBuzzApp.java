c0478b99f76e89d358697e3640e9b0d9a8b1a4db
Michael McNichol <mcnichol.m@gmail.com>
2016-09-14 07:30:31 -0500

    Test 7: Passed Integer 10, returns Buzz - Refactor

--- src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzApp.java
+++ src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzApp.java
@@ -5,14 +5,18 @@ public class FizzBuzzApp {
     public String execute(Integer userInput) {
         String result;
 
-        if (userInput % 3 == 0) {
+        if (isInputDivisibleBy(3, userInput)) {
             result = "Fizz";
-        } else if (userInput % 5 == 0) {
+        } else if (isInputDivisibleBy(5, userInput)) {
             result = "Buzz";
         } else {
             result = String.valueOf(userInput);
         }
 
         return result;
+    }
+
+    private boolean isInputDivisibleBy(Integer divisor, Integer userInput) {
+        return userInput % divisor == 0;
     }
 }
