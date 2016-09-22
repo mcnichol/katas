816631e7fb4e87d6da4cba51d3d96d817d55883e
Michael McNichol <mcnichol.m@gmail.com>
2016-09-21 22:00:04 -0500

    Test 12: Controller passed Integer, response message has String - Passing

--- src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzController.java
+++ src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzController.java
@@ -1,9 +1,14 @@
 package com.mcnichol.katas.fizzbuzz;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fizzbuzz")
 public class FizzBuzzController {
     private FizzBuzz fizzBuzz;
 
@@ -12,4 +17,9 @@ public class FizzBuzzController {
         this.fizzBuzz = fizzBuzz;
     }
 
+    @RequestMapping("/{input}")
+    public ResponseEntity<FizzBuzzResponse> getFizzBuzz(@PathVariable("input") Integer input){
+
+      return new ResponseEntity<>(new FizzBuzzResponse(""), HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/mcnichol/katas/fizzbuzz/FizzBuzzResponse.java
@@ -0,0 +1,17 @@
+package com.mcnichol.katas.fizzbuzz;
+
+public class FizzBuzzResponse {
+    private String response;
+
+    FizzBuzzResponse(String response){
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
